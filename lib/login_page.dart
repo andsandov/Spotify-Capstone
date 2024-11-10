3e8214d3aa8618463d2d6ffebbbb7614dcb09156
Eric Sun <ericsun987@gmail.com>
2024-11-10 05:47:52 -0800

    Added relevant packages and dart imports to login_page.dart pertaining to Spotify authentication.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,11 @@
+import 'dart:developer';
+import 'dart:html' as html;
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
+  // TODO replace current title with the name of the app
   const LoginPage({super.key, this.title = "Login Page"});
 
   final String title;
@@ -22,8 +27,9 @@ class _LoginPageState extends State<LoginPage> {
               style: const TextStyle(fontSize: 30),
             ),
             TextButton(
-                onPressed: () {},
-                child: const Text('test button')
+              // TODO onPressed should go to Spotify login from Spotify API
+              onPressed: () {},
+              child: const Text('test button')
             )
           ],
         ),
