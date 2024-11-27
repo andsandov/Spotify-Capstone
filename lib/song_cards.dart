708bd74aebdfde6fd443c6e49f980d75c15e3868
Eric Sun <eric.sun@plu.edu>
2024-11-27 05:54:07 -0800

    now generates cards based on elements in "songs" list

--- lib/song_cards.dart
+++ lib/song_cards.dart
@@ -16,22 +16,44 @@ class SongCards extends StatefulWidget {
 class _SongCardsState extends State<SongCards> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 550,
-      height: 650,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: Colors.blue[300],
-        borderRadius: BorderRadius.circular(40.0),
-      ),
-      child: Text(
-        "box 1",
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
+    const double boxWidth = 600;
+    const double boxHeight = 700;
+    const double overlapOffset = 20; // Vertical spacing for the stack
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+            width: boxWidth,
+            height: (5 * overlapOffset) + boxHeight,
+            child: Stack(
+              children: [
+                for (int i = 0; i < widget.songs.length; i++)
+                  Positioned(
+                    bottom: (i * 20), // Controls vertical offset
+                    child: Container(
+                      width: boxWidth,
+                      height: boxHeight,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Color.lerp(
+                            Colors.blue[300],
+                            Colors.blue[900],
+                            i / widget.songs.length),
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
+                      child: Text(
+                        "box 1",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+              ],
+            )),
+      ],
     );
   }
 }
