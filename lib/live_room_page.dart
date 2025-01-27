00d433a9cfbc2e4567a5b7ed48c5608dc9ec73b8
Eric Sun <eric.sun@plu.edu>
2025-01-26 16:00:12 -0800

    code for control bar moved into control_bar.dart. screen scaler also implemented.

--- lib/live_room_page.dart
+++ lib/live_room_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_polls/control_bar.dart';
 import 'package:spotify_polls/song_cards.dart';
 
 class LiveRoomPage extends StatefulWidget {
@@ -11,7 +12,6 @@ class LiveRoomPage extends StatefulWidget {
 }
 
 class _LiveRoomPageState extends State<LiveRoomPage> {
-  bool playState = false;
 
   final List<SongCardData> _songCards = [];
 
@@ -31,22 +31,7 @@ class _LiveRoomPageState extends State<LiveRoomPage> {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.sizeOf(context).width;
-    double screenHeight = MediaQuery.sizeOf(context).height;
-
-    const double aspectRatio = 2 / 3;
-
-    final maxHeight = screenHeight;
-    final maxWidth = maxHeight * aspectRatio;
-
-    final containerWidth = maxWidth > screenWidth ? screenWidth : maxWidth;
-    final containerHeight = containerWidth / aspectRatio;
-
-    void togglePlayState() {
-      setState(() {
-        playState = !playState;
-      });
-    }
+    double shortestSide = MediaQuery.sizeOf(context).shortestSide;
 
     void voteYes() {
       _songCards[_songCards.length - 1].votes[0] += 1;
@@ -54,16 +39,6 @@ class _LiveRoomPageState extends State<LiveRoomPage> {
     void voteNo() {
       _songCards[_songCards.length - 1].votes[1] += 1;
     }
-
-    ButtonStyle playerButtonStyle = ElevatedButton.styleFrom(
-        padding: EdgeInsets.all(containerHeight * 0.01),
-        minimumSize: const Size.square(1));
-
-    ButtonStyle queueButtonStyle = ElevatedButton.styleFrom(
-        padding: EdgeInsets.all(containerHeight * 0.01),
-        minimumSize: const Size.square(1),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white);
 
     return Scaffold(
       body: Column(
@@ -125,56 +100,7 @@ class _LiveRoomPageState extends State<LiveRoomPage> {
               ),
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                // Temporary Card container
-                padding: EdgeInsets.symmetric(vertical: containerHeight * 0.01),
-                margin: EdgeInsets.fromLTRB(
-                    (screenWidth * 0.5) - ((containerWidth * 0.7) / 2),
-                    containerHeight * 0.01,
-                    0,
-                    containerHeight * 0.01),
-                width: containerWidth * 0.7,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.indigoAccent,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Row(
-                  // Control bar
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                        style: playerButtonStyle,
-                        onPressed: () {},
-                        child: Icon(Icons.skip_previous,
-                            size: containerHeight * 0.06)),
-                    ElevatedButton(
-                      style: playerButtonStyle,
-                      onPressed: togglePlayState,
-                      child: Icon(playState ? Icons.pause : Icons.play_arrow,
-                          size: containerHeight * 0.06),
-                    ),
-                    ElevatedButton(
-                        style: playerButtonStyle,
-                        onPressed: () {},
-                        child: Icon(Icons.skip_next,
-                            size: containerHeight * 0.06)),
-                  ],
-                ),
-              ),
-              Container(
-                alignment: Alignment.bottomRight,
-                child: ElevatedButton(
-                    style: queueButtonStyle,
-                    onPressed: () {},
-                    child:
-                        Icon(Icons.queue_music, size: containerHeight * 0.06)),
-              )
-            ],
-          )
+          ControlBar(size: shortestSide)
         ],
       ),
     );
