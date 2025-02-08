3573bb4fa070b3a2323272506898460d069eff53
Eric Sun <eric.sun@plu.edu>
2025-02-07 22:19:45 -0800

    Created VotelistItem to represent Votelists in the Votelists page.

---
+++ lib/votelist_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_polls/media_items.dart';
+import 'package:spotify_polls/voting_page.dart';
+
+class VotelistItem extends StatefulWidget {
+  const VotelistItem ({super.key,
+  required this.itemData});
+
+  final MediaItemData itemData;
+
+  @override
+  State<StatefulWidget> createState() => _VotelistItemState();
+}
+
+class _VotelistItemState extends State<VotelistItem> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const VotingPage()));
+      },
+      child: MediaItem(data: widget.itemData),
+    );
+  }
+}
