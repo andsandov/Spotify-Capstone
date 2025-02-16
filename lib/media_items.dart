1b373cd122985683e1c2f773e1b0f59242685c11
Eric Sun <eric.sun@plu.edu>
2025-02-16 01:23:22 -0800

    Default image given for media items with no image.

--- lib/media_items.dart
+++ lib/media_items.dart
@@ -82,7 +82,7 @@ class MediaItemData {
   const MediaItemData ({
     required this.title,
     required this.details,
-    required this.imageUrl,
+    this.imageUrl = "https://th.bing.com/th/id/R.e78f8e7c326d3e7cdcf053d58f494542?rik=bXopo7rm0XIdFQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fc%2fc7%2fDomestic_shorthaired_cat_face.jpg&ehk=NByReFekRNa%2fCe0v9gNPEb0tpYmVhy4kI5uaC1l1AUI%3d&risl=1&pid=ImgRaw&r=0",
   });
 
   final String title;
