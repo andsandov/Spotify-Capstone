5f2b01a552821ee761d9efd58bd37f3332203e5a
Eric Sun <eric.sun@plu.edu>
2025-02-17 01:59:57 -0800

    Dummy data for SongQueue component.

--- lib/pages/live_room_page.dart
+++ lib/pages/live_room_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_polls/control_bar.dart';
 import 'package:spotify_polls/song_cards.dart';
+import '../media_items.dart';
 
 class LiveRoomPage extends StatefulWidget {
   const LiveRoomPage({super.key, this.title = "Live Room Page"});
@@ -12,6 +13,19 @@ class LiveRoomPage extends StatefulWidget {
 }
 
 class _LiveRoomPageState extends State<LiveRoomPage> {
+  final List<MediaItemData> liveQueueData = [
+    const MediaItemData(
+        title: "song1",
+        details: "song1 details",
+        imageUrl:
+        'https://th.bing.com/th/id/R.e78f8e7c326d3e7cdcf053d58f494542?rik=bXopo7rm0XIdFQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fc%2fc7%2fDomestic_shorthaired_cat_face.jpg&ehk=NByReFekRNa%2fCe0v9gNPEb0tpYmVhy4kI5uaC1l1AUI%3d&risl=1&pid=ImgRaw&r=0'),
+    const MediaItemData(
+        title: "song2",
+        details: "song2 details",
+        imageUrl:
+        'https://static.scientificamerican.com/sciam/cache/file/2AE14CDD-1265-470C-9B15F49024186C10_source.jpg?w=1200'),
+  ];
+
   final List<SongCardData> _songCards = [];
 
   void _addSong() {
